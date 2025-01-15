a71743f8fe7ef8bdfc1f5149b45f300d98a1723a
arlocomotive <113862557+arlocomotive@users.noreply.github.com>
2025-01-15 10:57:48 -0600

    wrong folder

---
+++ main.js
@@ -0,0 +1,23 @@
+previewImg = document.querySelector(".preview-img img");
+saveImgBtn = document.querySelector(".save-img");
+
+const saveImage = () => {
+    const canvas = document.createElement("canvas");
+    const context = canvas.getContext("2d");
+    canvas.width = previewImg.naturalWidth;
+    canvas.height = previewImg.naturalHeight;
+
+    let img = document.createElement("img");
+    img.setAttribute("crossorigin", "anonymous")
+    img.onload = function() {
+        context.drawImage(previewImg, 0, 0);
+        context.drawImage(img, 0, 0, canvas.width, canvas.height * .5);
+        const link = document.createElement("a");
+        link.download = "image.png";
+        link.href = canvas.toDataURL();
+        link.click();
+    }
+    img.src = "thy end.png";
+}
+
+saveImgBtn.addEventListener("click", saveImage);
