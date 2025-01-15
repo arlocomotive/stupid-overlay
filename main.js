fa40d1d5720e31122963a5c1cada418a2acfec9c
arlocomotive <113862557+arlocomotive@users.noreply.github.com>
2025-01-15 14:27:14 -0600

    zoom #32385832

--- main.js
+++ main.js
@@ -38,7 +38,7 @@ baseImage.onload = function() {
     render();
     avatarImage.onload = () => {
         context.drawImage(baseImage, 0, 0);
-        const offset = (size * zoom)
+        const offset = (size * ((1 + zoom) * .5))
         const zoom_size = (size * (1 + zoom))
         context.drawImage(avatarImage, offset, offset, zoom_size, zoom_size, 0, 155, 320, 160);
         context.drawImage(avatarImage, offset, offset, zoom_size, zoom_size, 0, 455, 320, 160);
