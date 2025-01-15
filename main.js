0e4c43abc33d0bb1f87bcdef641ee05c93d88ad5
arlocomotive <113862557+arlocomotive@users.noreply.github.com>
2025-01-15 12:45:24 -0600

    canvas

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 saveImageButton = document.querySelector(".save-image");
+previewCanvas = document.querySelector(".preview-canvas");
 userIDInput = document.querySelector(".roblox-userid");
+previewContext = previewCanvas.getContext("2d");
 
 async function fetchAsync (url) {
     let response = await fetch(url);
@@ -10,21 +12,19 @@ async function fetchAsync (url) {
 let base_image = document.createElement("img");
 base_image.setAttribute("crossorigin", "anonymous");
 base_image.onload = function() {
+    previewCanvas.width = base_image.naturalWidth;
+    previewCanvas.height = base_image.naturalHeight;
+
     let avatar_image = document.createElement("img");
     avatar_image.setAttribute("crossorigin", "anonymous");
     avatar_image.onload = function() {
         console.log("loaded");
-        const canvas = document.createElement("canvas");
-        const context = canvas.getContext("2d");
-        canvas.width = base_image.naturalWidth;
-        canvas.height = base_image.naturalHeight;
-    
-        context.drawImage(base_image, 0, 0);
-        context.drawImage(avatar_image, 0, 155, 320, 160);
-        context.drawImage(avatar_image, 0, 455, 320, 160);
+        previewContext.drawImage(base_image, 0, 0);
+        previewContext.drawImage(avatar_image, 0, 155, 320, 160);
+        previewContext.drawImage(avatar_image, 0, 455, 320, 160);
         const link = document.createElement("a");
         link.download = "image.png";
-        link.href = canvas.toDataURL();
+        link.href = previewCanvas.toDataURL();
         link.click();
     }
     saveImageButton.addEventListener("click", () => {
