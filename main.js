7a093509ac5df79b4889720e0614407ef82880f7
arlocomotive <113862557+arlocomotive@users.noreply.github.com>
2025-01-15 11:44:43 -0600

    lets see if I broke anything!

--- main.js
+++ main.js
@@ -1,25 +1,39 @@
-previewImg = document.querySelector(".preview-img img");
-saveImgBtn = document.querySelector(".save-img");
+saveImageButton = document.querySelector(".save-image");
+userIDInput = document.querySelector(".roblox-userid");
 
-console.log("nananana")
-let base_image = document.createElement("img");
-base_image.setAttribute("crossorigin", "anonymous")
+async function fetchAsync (url) {
+    let response = await fetch(url);
+    let data = await response.json();
+    return data;
+}
+
+function createImage() {
+    let new_image = document.createElement("img");
+    new_image.setAttribute("crossorigin", "anonymous");
+    return new_image;
+}
+
+let avatar_image = createImage();
+let base_image = createImage();
 
 const saveImage = () => {
-    const canvas = document.createElement("canvas");
-    const context = canvas.getContext("2d");
-    canvas.width = base_image.naturalWidth;
-    canvas.height = base_image.naturalHeight;
+    let data = fetchAsync(("https://thumbnails.roblox.com/v1/users/avatar-headshot?userIds=" + userIDInput.value + "&size=420x420&format=Png"))
+    console.log(data)
+    // const canvas = document.createElement("canvas");
+    // const context = canvas.getContext("2d");
+    // canvas.width = base_image.naturalWidth;
+    // canvas.height = base_image.naturalHeight;
 
-    context.drawImage(base_image, 0, 0);
-    context.drawImage(previewImg, 0, 155, 320, 160);
-    const link = document.createElement("a");
-    link.download = "image.png";
-    link.href = canvas.toDataURL();
-    link.click();
+    // context.drawImage(base_image, 0, 0);
+    // context.drawImage(avatar_image, 0, 155, 320, 160);
+    // context.drawImage(avatar_image, 0, 455, 320, 160);
+    // const link = document.createElement("a");
+    // link.download = "image.png";
+    // link.href = canvas.toDataURL();
+    // link.click();
 }
 
 base_image.onload = function() {
-    saveImgBtn.addEventListener("click", saveImage);
+    saveImageButton.addEventListener("click", saveImage)
 }
 base_image.src = "thy start.png";
