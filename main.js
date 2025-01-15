ae938b27faccd9b6429ea091358252c3723633a3
arlocomotive <113862557+arlocomotive@users.noreply.github.com>
2025-01-15 13:44:32 -0600

    realtime stupidity

--- main.js
+++ main.js
@@ -10,7 +10,6 @@ async function fetchAsync (url) {
 const baseImage = document.createElement("img");
 baseImage.setAttribute("crossorigin", "anonymous");
 baseImage.onload = function() {
-
     const canvas = document.createElement("canvas");
     const context = canvas.getContext("2d");
     canvas.width = baseImage.naturalWidth;
@@ -21,20 +20,22 @@ baseImage.onload = function() {
 
     const render = () => {
         const input = userIDInput.value.replace(/\D/g, "")
-        lastInput = input
-        if (input && (input != lastInput)) {
-            fetchAsync(("https://thumbnails.roproxy.com/v1/users/avatar-headshot?userIds=" + input + "&size=420x420&format=Png")).then(function(response) {
-                const data = response.data;
-                if (data && data[0]) {
-                    avatarImage.src = data[0].imageUrl;
-                }
-            })
-            context.drawImage(baseImage, 0, 0);
-            context.drawImage(avatarImage, 0, 155, 320, 160);
-            context.drawImage(avatarImage, 0, 455, 320, 160);
-            const dataURL = canvas.toDataURL();
-            previewImage.src = dataURL;
-            return dataURL;
+        if (input != lastInput) {
+            lastInput = input;
+            if (input) {
+                fetchAsync(("https://thumbnails.roproxy.com/v1/users/avatar-headshot?userIds=" + input + "&size=420x420&format=Png")).then(function(response) {
+                    const data = response.data;
+                    if (data && data[0]) {
+                        avatarImage.src = data[0].imageUrl;
+                    }
+                })
+                context.drawImage(baseImage, 0, 0);
+                context.drawImage(avatarImage, 0, 155, 320, 160);
+                context.drawImage(avatarImage, 0, 455, 320, 160);
+                const dataURL = canvas.toDataURL();
+                previewImage.src = dataURL;
+                return dataURL;
+            }
         }
     }
     saveImageButton.addEventListener("click", () => {
