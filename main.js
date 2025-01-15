437f0bc6f78f30de04140d5c8aa6dd263a4d053a
arlocomotive <113862557+arlocomotive@users.noreply.github.com>
2025-01-15 14:40:12 -0600

    hardcode

--- main.js
+++ main.js
@@ -53,7 +53,7 @@ baseImage.onload = function() {
         }
     }
     avatarZoom.addEventListener("input", () => {
-        zoom = (avatarZoom.value / (avatarZoom.max + 1));
+        zoom = (avatarZoom.value / 21);
     })
     saveButton.addEventListener("click", () => {
         save = true;
