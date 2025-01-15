9d81662ba100bcb42cb4d8aca0f82967fe995306
arlocomotive <113862557+arlocomotive@users.noreply.github.com>
2025-01-15 11:51:18 -0600

    wasnt my fault

--- main.js
+++ main.js
@@ -17,22 +17,25 @@ let avatar_image = createImage();
 let base_image = createImage();
 
 const saveImage = () => {
-    let data = fetchAsync(("https://thumbnails.roblox.com/v1/users/avatar-headshot?userIds=" + userIDInput.value + "&size=420x420&format=Png"))
-    console.log(data)
-    // const canvas = document.createElement("canvas");
-    // const context = canvas.getContext("2d");
-    // canvas.width = base_image.naturalWidth;
-    // canvas.height = base_image.naturalHeight;
-
-    // context.drawImage(base_image, 0, 0);
-    // context.drawImage(avatar_image, 0, 155, 320, 160);
-    // context.drawImage(avatar_image, 0, 455, 320, 160);
-    // const link = document.createElement("a");
-    // link.download = "image.png";
-    // link.href = canvas.toDataURL();
-    // link.click();
+    fetchAsync(("https://thumbnails.roproxy.com/v1/users/avatar-headshot?userIds=" + userIDInput.value + "&size=420x420&format=Png")).then(function(response) {
+        avatar_image.src = response.data[0].imageUrl
+    })
 }
 
+avatar_image.onload = function() {
+    const canvas = document.createElement("canvas");
+    const context = canvas.getContext("2d");
+    canvas.width = base_image.naturalWidth;
+    canvas.height = base_image.naturalHeight;
+
+    context.drawImage(base_image, 0, 0);
+    context.drawImage(avatar_image, 0, 155, 320, 160);
+    context.drawImage(avatar_image, 0, 455, 320, 160);
+    const link = document.createElement("i");
+    link.download = "image.png";
+    link.href = canvas.toDataURL();
+    link.click();
+}
 base_image.onload = function() {
     saveImageButton.addEventListener("click", saveImage)
 }
